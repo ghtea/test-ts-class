4d6788132621ff2f9961403070863ec49363d125
Jaehyeon Park <47841931+ghtea@users.noreply.github.com>
2020-10-11 22:52:16 +0900

    changed to class components

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,39 @@
 import * as React from "react";
 import "./styles.css";
 
-export default function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
+type PropsApp = {};
+type StateApp = {
+  time: Date;
+};
+
+class App extends React.Component<PropsApp, StateApp> {
+  state: StateApp = {
+    // optional second annotation for better type inference
+    time: new Date()
+  };
+
+  // The tick function sets the current state. TypeScript will let us know
+  // which ones we are allowed to set.
+  tick() {
+    this.setState({
+      time: new Date()
+    });
+  }
+
+  // Before the component mounts, we initialise our state
+  componentWillMount() {
+    this.tick();
+  }
+
+  // After the component did mount, we set the state each second.
+  componentDidMount() {
+    setInterval(() => this.tick(), 1000);
+  }
+
+  // render will know everything!
+  render() {
+    return <p>The current time is {this.state.time.toLocaleTimeString()}</p>;
+  }
 }
+
+export default App;
